47333d8c40b0f1130b0ca7da269a5f6b67c00fba
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-05-28 12:37:05 -0700

    challenge 1 and 2 complete

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 var fs = require('fs');
+var path = require('path');
+
+// Challenge 1
+
+console.log(fs.readFileSync(path.join('.','challenge1', 'info.txt')).toString())
+
+// Challenge 2
+
+let oldText = fs.readFileSync(path.join('.', 'challenge2', 'info.txt')).toString()
+oldText += 'Raffi Shahbazian'
+fs.writeFileSync(path.join('.', 'challenge2', 'info.txt'), oldText)
+// Challenge 3
+
+// Challenge 4
+
+// Challenge 5
+
+// Challenge 6
