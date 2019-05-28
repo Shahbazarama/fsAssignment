01503259d08a22248322aadc94218c3c025672c5
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-05-28 13:07:44 -0700

    run 'node app.js' to watch all the challenges complete themselves.

--- app.js
+++ app.js
@@ -7,24 +7,24 @@ console.log(fs.readFileSync(path.join('.','challenge1', 'info.txt')).toString())
 
 // Challenge 2
 
-// let oldText = fs.readFileSync(path.join('.', 'challenge2', 'info.txt')).toString()
-// oldText += 'Raffi Shahbazian'
-// fs.writeFileSync(path.join('.', 'challenge2', 'info.txt'), oldText)
+let oldText = fs.readFileSync(path.join('.', 'challenge2', 'info.txt')).toString()
+oldText += 'Raffi Shahbazian'
+fs.writeFileSync(path.join('.', 'challenge2', 'info.txt'), oldText)
 
 // Challenge 3
 
-// fs.renameSync(path.join('.', 'challenge3', 'binfo.txt'), path.join('.', 'challenge3', 'info.txt'))
+fs.renameSync(path.join('.', 'challenge3', 'binfo.txt'), path.join('.', 'challenge3', 'info.txt'))
 
 // Challenge 4
 
-// fs.mkdirSync(path.join('.', 'challenge4', 'copyfolder'))
-// fs.copyFileSync(path.join('.', 'challenge4', 'info.txt'), path.join('.', 'challenge4', 'copyfolder', 'info.txt'))
+fs.mkdirSync(path.join('.', 'challenge4', 'copyfolder'))
+fs.copyFileSync(path.join('.', 'challenge4', 'info.txt'), path.join('.', 'challenge4', 'copyfolder', 'info.txt'))
 
 // Challenge 5
 
-// let oldString = fs.readFileSync(path.join('.', 'challenge5', 'info.txt')).toString()
-// oldString = oldString.replace(/-/g, ' ')
-// fs.writeFileSync(path.join('.', 'challenge5', 'info.txt'), oldString)
+let oldString = fs.readFileSync(path.join('.', 'challenge5', 'info.txt')).toString()
+oldString = oldString.replace(/-/g, ' ')
+fs.writeFileSync(path.join('.', 'challenge5', 'info.txt'), oldString)
 
 // Challenge 6
 
