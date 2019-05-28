d7b0b7c3c2c4e2d9576a8ee9e3f34e4b904a998e
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-05-28 12:42:52 -0700

    challenges 3 and 4 complete

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ console.log(fs.readFileSync(path.join('.','challenge1', 'info.txt')).toString())
 
 // Challenge 2
 
-let oldText = fs.readFileSync(path.join('.', 'challenge2', 'info.txt')).toString()
-oldText += 'Raffi Shahbazian'
-fs.writeFileSync(path.join('.', 'challenge2', 'info.txt'), oldText)
+// let oldText = fs.readFileSync(path.join('.', 'challenge2', 'info.txt')).toString()
+// oldText += 'Raffi Shahbazian'
+// fs.writeFileSync(path.join('.', 'challenge2', 'info.txt'), oldText)
+
 // Challenge 3
 
+// fs.renameSync(path.join('.', 'challenge3', 'binfo.txt'), path.join('.', 'challenge3', 'info.txt'))
+
 // Challenge 4
+
+fs.mkdirSync(path.join('.', 'challenge4', 'copyfolder'))
+fs.copyFileSync(path.join('.', 'challenge4', 'info.txt'), path.join('.', 'challenge4', 'copyfolder', 'info.txt'))
 
 // Challenge 5
 
